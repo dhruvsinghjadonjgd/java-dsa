9d77c8d0ad89690ea7bde55380f389a4027bbe2c
Dhruv Singh Jadon <dhruvsinghjadonjgd@gmail.com>
2026-06-30 10:16:12 +0530

    Create Pattern5.java

---
+++ Pattern Printing/Pattern5.java
@@ -0,0 +1,25 @@
+import java.util.*;;
+public class practice {
+    // print numbered square
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for (int i=1; i < n+1 ; i++ ) {
+            for (int j =0;j < n; j++ ) {
+                System.out.print((char)(j +65));
+            }
+            System.out.println(""); // Move to the next line after each row
+        }
+
+        }
+
+    }
+
+output:
+ABCDE
+ABCDE
+ABCDE
+ABCDE
+ABCDE
