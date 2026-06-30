a86f47866df20b44008ea15efe3bcc26ab7a9aa4
Dhruv Singh Jadon <dhruvsinghjadonjgd@gmail.com>
2026-06-30 10:30:43 +0530

    Update Pattern6.java

--- Pattern Printing/Pattern6.java
+++ Pattern Printing/Pattern6.java
@@ -26,3 +26,32 @@ Output
 *****
 
 */
+
+import java.util.*;;
+public class practice {
+    // print numbered square
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+          int r = sc.nextInt();
+            
+        for (int i=1; i <= r ; i++ ) {
+            for (int j = 1;j <= i; j++ ) {
+                System.out.print(j);
+            }
+            System.out.println(""); // Move to the next line after each row
+        }
+
+        }
+
+    }
+
+/*
+Output
+1
+12
+123
+1234
+12345
+
+*/
